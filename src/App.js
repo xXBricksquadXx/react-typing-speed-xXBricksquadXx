c447bd5b2136116fb54b3f3a388151e88b8c2c35
colby <colby88cardell@gmail.com>
2021-03-05 19:47:11 -0500

    :seedling: starter code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Header, Main } from "./components";
+import { Main } from "./components";
 
 function App() {
   return (
-    <div className="flex flex-col font-mono gap-4 text-red-500">
-      <Header />
+    <div className="flex flex-col font-mono gap-4 text-neon-green">
+      <header className="pt-4 text-7xl text-center">Typing Speed Test</header>
       <Main />
     </div>
   );
--- src/components/Header/index.js
+++
@@ -1,7 +0,0 @@
-function Header() {
-  return (
-    <header className="pt-4 text-7xl text-center">Typing Speed Test</header>
-  );
-}
-
-export default Header;
